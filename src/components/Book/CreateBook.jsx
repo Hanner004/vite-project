cc8497ff99094bcf3c1d0b563b1610ade617067e
Hanner Enrique De La Hoz Barraza <hdelahoz@lya-electronic.com>
2023-05-04 16:18:13 -0500

    [ADD] add book

--- src/components/Book/CreateBook.jsx
+++ src/components/Book/CreateBook.jsx
@@ -42,32 +42,40 @@ export default function CreateBook() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch(booksAPI, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: bookName,
-        available_quantity: bookAvailableQuantity,
-        library_location: bookLibraryLocation,
-        authorId: authorOption.author_id,
-        editorialId: editorialOption.editorial_id,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error.message))
-      .finally(() => {
-        Swal.fire({
-          icon: "success",
-          title: "Libro agregado",
-          text: "Libro agregado correctamente.",
-          showConfirmButton: false,
-          timer: 2000,
-        });
-        navigate("/book");
+    if (!authorOption.author_id || !editorialOption.editorial_id) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "El autor y editorial son requeridos",
       });
+    } else {
+      fetch(booksAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: bookName,
+          available_quantity: Number(bookAvailableQuantity),
+          library_location: bookLibraryLocation,
+          authorId: authorOption.author_id,
+          editorialId: editorialOption.editorial_id,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error.message))
+        .finally(() => {
+          Swal.fire({
+            icon: "success",
+            title: "Libro agregado",
+            text: "Libro agregado correctamente.",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+          navigate("/book");
+        });
+    }
   }
 
   const handleCancel = () => {
@@ -119,18 +127,18 @@ export default function CreateBook() {
               />
             </div>
             <div className="row mb-3">
-              <div className="col-2">
+              <div className="col-md-4">
                 <EditorialDropdown editorials={editorials} editorialOptionClick={(option) => editorialOptionClick(option)} />
               </div>
-              <div className="col-10">
+              <div className="col-md-8">
                 <input type="text" className="form-control" placeholder="Editorial" required disabled value={editorialOption.editorial_name} />
               </div>
             </div>
             <div className="row mb-3">
-              <div className="col-2">
+              <div className="col-md-4">
                 <AuthorDropdown authors={authors} authorOptionClick={(option) => authorOptionClick(option)} />
               </div>
-              <div className="col-10">
+              <div className="col-md-8">
                 <input type="text" className="form-control" placeholder="Autor" required disabled value={authorOption.author_name} />
               </div>
             </div>
@@ -151,6 +159,7 @@ export default function CreateBook() {
 
   function editorialOptionClick(option) {
     setEditorialOption(option);
+    console.log(option);
   }
 
   function authorOptionClick(option) {
