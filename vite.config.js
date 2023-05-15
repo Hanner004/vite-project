433f47189112a8c3527aad9057f577ad98e75e51
Hanner Enrique De La Hoz Barraza <hdelahoz@lya-electronic.com>
2023-05-15 14:50:44 -0500

    [UPDATE]

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    cors: true,
-    host: '0.0.0.0',
-  },
+  server: { open: true, origin:'*' },
 });
