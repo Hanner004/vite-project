a6100eda063f507238c2993b6a6a7135ea43b33a
Hanner Enrique De La Hoz Barraza <hdelahoz@lya-electronic.com>
2023-06-18 15:39:09 -0500

    [ADD] home carousel

---
+++ src/components/Carousel-Home/Carousel-Home.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import './Carousel-Home.css';
+
+export default function Carousel_Home() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <div className="carousel-item-content">
+          <img
+            className="d-block w-100"
+            src="/carousel-images/slide1.jpg"
+            alt="First slide"
+          />
+          <div className="carousel-caption">
+            <h3>Conozca nuestra plataforma de libros</h3>
+            {/* <p>
+              Explora nuestra plataforma y accede a una amplia variedad de
+              libros.
+            </p> */}
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="carousel-item-content">
+          <img
+            className="d-block w-100"
+            src="/carousel-images/slide2.jpg"
+            alt="Second slide"
+          />
+          <div className="carousel-caption">
+            <h3>Explora nuestro catálogo de libros</h3>
+            {/* <p>
+              Descubre una amplia selección de libros en nuestra plataforma.
+              Encuentra tu próxima lectura y sumérgete en un mundo de
+              conocimiento.
+            </p> */}
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="carousel-item-content">
+          <img
+            className="d-block w-100"
+            src="/carousel-images/slide3.jpg"
+            alt="Third slide"
+          />
+          <div className="carousel-caption">
+            <h3>Explora los senderos de la sabiduría literaria</h3>
+            {/* <p>
+              Embárcate en un viaje inolvidable a través de nuestra plataforma
+              de libros. Descubre tesoros literarios, sumérgete en emocionantes
+              tramas y despierta tu imaginación. ¡Únete a nuestra expedición
+              literaria y conquista nuevos horizontes de conocimiento!
+            </p> */}
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
