a292dde0b5d52bf2e40ac7ead6a817a8a88d456b
Hanner Enrique De La Hoz Barraza <hdelahoz@lya-electronic.com>
2023-07-04 13:40:18 -0500

    [ADD] book color

--- src/components/Home/Home-Book.jsx
+++ src/components/Home/Home-Book.jsx
@@ -54,8 +54,9 @@ export default function HomeBook() {
               <img src="/book.jpg" className="card-img-top" alt={item.book_name} />
               <div className="card-body">
                 <h5 className="card-title">
-                  #{item.book_id} - {item.book_name}&nbsp;
-                  {circleBookClass(item.book_available_quantity - item.book_current_amount_occupied)}
+                  #{item.book_id}&nbsp;
+                  {circleBookClass(item.book_available_quantity - item.book_current_amount_occupied)} -&nbsp;
+                  {item.book_name}
                 </h5>
                 <p className="card-text">
                   <small>
